192bb88b32119a94bd1024aa71d05d824131e4a7
Tatiana Anuchina <1103atv@gmail.com>
2024-08-05 22:44:21 +1000

    add docker version checking

--- start.sh
+++ start.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-
 echo "script was started"
+
 sudo apt install curl
+docker_v=$(docker -v)
+if [[ $docker_v =~ "version" ]]
+then
+echo "docker already installed, proceed to the next step..."
+else
+echo "docker not found, start installing..."
 sudo curl https://get.docker.com -o install.sh && sh install.sh
 systemctl enable docker.service
 systemctl enable docker
 usermod -aG docker $USER
-echo "docker installed"
+echo "docker was installed"
+fi
 
 cd ~ && curl -LO https://github.com/atv1103/wireguard/archive/master.tar.gz && tar -xvf master.tar.gz && rm master.tar.gz 
 cd wireguard-master
@@ -14,4 +21,5 @@ cd wireguard-master
 
 mkdir -p /app/wireguard
 cp ./docker-compose.yml /app/wireguard
+
 echo "script was ended. Well done!"
